bd3988d825f44bfaf28a9d1f517d37585c750345
jangky000 <jangky000@mme.dongguk.edu>
2020-09-28 20:31:11 +0900

    eslint prettier 적용

---
+++ server/.eslintrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+  env: {
+    browser: true,
+    commonjs: true,
+    es2021: true,
+    node: true,
+  },
+  extends: ["eslint:recommended", "plugin:prettier/recommended"],
+  parser: "babel-eslint",
+  parserOptions: {
+    ecmaVersion: 12,
+  },
+  rules: {
+    "prettier/prettier": [
+      "error",
+      {
+        endOfLine: "auto",
+      },
+    ],
+    "no-unused-vars": "warn",
+    "no-console": "off",
+    "func-names": "off",
+    "no-process-exit": "off",
+    "object-shorthand": "off",
+    "class-methods-use-this": "off",
+  },
+};
